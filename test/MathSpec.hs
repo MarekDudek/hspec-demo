7b696650f70e7772fd336d74d9c0713e1a933fa6
Marek Dudek <marek.dudek@gmail.com>
2017-09-20 13:52:11 +0200

    automatic discovery of test suites

--- test/MathSpec.hs
+++ test/MathSpec.hs
@@ -1,8 +1,15 @@
+module MathSpec where
+
+
 import Test.Hspec
 import Math
 
+
 main :: IO ()
-main = hspec $ do
+main = hspec spec
+
+spec :: Spec
+spec = do
     describe "absolute" $ do
         it "returns the original number when given a positive input" $
           absolute 1 `shouldBe` 1
