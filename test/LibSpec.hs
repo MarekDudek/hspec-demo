a9bb94b10d2e85e71bfc3b38e989bf05159493fa
Marek Dudek <marek.dudek@gmail.com>
2017-09-23 12:23:55 +0200

    HLint suggestings applied

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -10,13 +10,11 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
+spec = 
   describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
+    it "returns the first element of a list" $ 
       head [23 ..] `shouldBe` (23 :: Int)
-
     it "returns the first element of an *arbitrary* list" $
       property $ \x xs -> head (x:xs) == (x :: Int)
-
-    it "throws an exception if used with an empty list" $ do
+    it "throws an exception if used with an empty list" $ 
       evaluate (head []) `shouldThrow` anyException
